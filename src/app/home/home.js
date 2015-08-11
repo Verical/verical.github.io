afbf6c6155578969abc4533ba5277e7d5dabc6e9
Tony Smith <tsmith@verical.com>
2015-08-11 12:14:11 -0600

    Init the new design

---
+++ src/app/home/home.js
@@ -0,0 +1,22 @@
+angular.module( 'ngBoilerplate.home', [
+  'ui.router',
+  'plusOne'
+])
+
+.config(["$stateProvider", function config( $stateProvider ) {
+  $stateProvider.state( 'home', {
+    url: '/home',
+    views: {
+      "main": {
+        controller: 'HomeCtrl',
+        templateUrl: '/src/app/home/home.tpl.html'
+      }
+    },
+    data:{ pageTitle: 'Home' }
+  });
+}])
+
+.controller( 'HomeCtrl', ["$scope", function HomeController( $scope ) {
+  
+}]);
+
