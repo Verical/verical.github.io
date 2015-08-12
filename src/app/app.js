6b6fcaeb2f12baa8f32e3b26f7cc5f921c077ee7
Tony Smith <tsmith@verical.com>
2015-08-12 08:41:14 -0600

    Adding feature list

--- src/app/app.js
+++ src/app/app.js
@@ -1,23 +1,15 @@
-angular.module( 'ngBoilerplate', [
-  'ngBoilerplate.home',
-  'ngBoilerplate.about',
-  'ui.router'
-])
+angular.module('ngBoilerplate', ['ngBoilerplate.home','ui.router','ngDropover'])
 
-.config( ["$stateProvider", "$urlRouterProvider", function myAppConfig ( $stateProvider, $urlRouterProvider ) {
-  $urlRouterProvider.otherwise( '/home' );
+.config(["$stateProvider", "$urlRouterProvider", function myAppConfig($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home');
 }])
 
-.run( function run () {
-})
+.run(function run() {})
 
-.controller( 'AppCtrl', ["$scope", "$location", function AppCtrl ( $scope, $location ) {
-  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
-    if ( angular.isDefined( toState.data.pageTitle ) ) {
-      $scope.pageTitle = toState.data.pageTitle + ' | ngBoilerplate' ;
-    }
-  });
-}])
-
-;
-
+.controller('AppCtrl', ["$scope", "$location", function AppCtrl($scope, $location) {
+    $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+        if (angular.isDefined(toState.data.pageTitle)) {
+            $scope.pageTitle = toState.data.pageTitle + ' | ngBoilerplate';
+        }
+    });
+}]);
--- src/app/home/home.js
+++ src/app/home/home.js
@@ -1,22 +1,34 @@
-angular.module( 'ngBoilerplate.home', [
-  'ui.router',
-  'plusOne'
+angular.module('ngBoilerplate.home', [
+    'ui.router',
+    'plusOne'
 ])
 
-.config(["$stateProvider", function config( $stateProvider ) {
-  $stateProvider.state( 'home', {
-    url: '/home',
-    views: {
-      "main": {
-        controller: 'HomeCtrl',
-        templateUrl: '/src/app/home/home.tpl.html'
-      }
-    },
-    data:{ pageTitle: 'Home' }
-  });
+.config(["$stateProvider", function config($stateProvider) {
+    $stateProvider.state('home', {
+        url: '/home',
+        views: {
+            "main": {
+                controller: 'HomeCtrl',
+                templateUrl: '/src/app/home/home.tpl.html'
+            }
+        },
+        data: {
+            pageTitle: 'Home'
+        }
+    });
 }])
 
-.controller( 'HomeCtrl', ["$scope", function HomeController( $scope ) {
-  
-}]);
+.controller('HomeCtrl', ["$scope", function HomeController($scope) {
 
+    $scope.exampleOptions = {
+        'offsetX': 0,
+        'offsetY': 0,
+        'wrapperClass': '',
+        'closeOthersOnOpen': false,
+        'trigger': "",
+        'triggerEvent': "click",
+        'position': "bottom-left",
+        'closeOnClickOff': true
+    };
+
+}]);
